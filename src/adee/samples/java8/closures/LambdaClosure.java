8d174b73c8e6989c025faeda16218e5be30733ba
Anmol Deep <anmoldeep0123@gmail.com>
2018-11-04 13:14:45 +0530

    closure

---
+++ src/adee/samples/java8/closures/LambdaClosure.java
@@ -0,0 +1,27 @@
+package adee.samples.java8.closures;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LambdaClosure {
+
+	public static void main(String[] args) {
+
+		int effectivelyFinalInt = 5;
+		int[] effectivelyFinalArr = { 1, 2, 3, 4, 5 };
+		List<String> names = new ArrayList<>();
+		names.add("Anmol");
+		names.add("BooBoo");
+		names.add("Ash");
+		names.add("Baba");
+
+		names = names.stream().filter(p -> {
+			effectivelyFinalInt = 4; // compile time error
+			// Local variable effectivelyFinal defined in an enclosing scope must be final
+			// or effectively final
+			effectivelyFinalArr[4] = 0; // is allowed coz for final array reference should not be changed
+			return p.length() == 5;
+		}).collect(Collectors.toList());
+	}
+}
